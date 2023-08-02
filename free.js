810e0f1fe51fdd14fc017ccf5bcc0cd3caa86370
fasihhaider <fasihhaider.xwave@gmail.com>
2023-08-02 10:13:52 +0500

    first commit

---
+++ free.js
@@ -0,0 +1,66 @@
+// Date:1/8/2023
+//  let div = document.createElement("h1");
+
+//  div.setAttribute("id", "frre");
+
+//  div.textContent = "Fasih";
+
+//  document.body.appendChild(div);
+
+//  let input = document.createElement("input");
+
+//  input.placeholder = ("Enter your name");
+
+//  div.textContent = "Fasih";
+
+//  console.log(input);
+
+//  document.body.appendChild(input);
+
+//  let getdiv = document.querySelector("#frre")
+ 
+//  getdiv.style.color = "aqua";
+
+// console.log(getdiv);
+
+// function checkuserAge(name, age){
+//     console.log('${name} is ${age} years old');
+// } 
+// checkuserAge("Fasih",18)
+
+// const user = {
+//     age:18,
+//     name:"fasih",
+
+// }
+
+// const fruitsArr1 = ["apple", "banana"];
+// const fruitsArr2 = ["grapes", "kiwi"];
+
+// console.log(fruits.push("grapes"));
+// console.log(fruits.pop());
+// console.log(fruits.shift());
+// console.log(fruits.unshift("grapes"));
+// console.log(fruitsArr1.slice(0));
+
+// console.log(fruitsArr1);
+
+// console.log(fruitsArr2);
+
+// Date:2/8/2023
+
+// const fruitsArr1 = ["apple", "banana", "kiwi"];
+
+// fruitsArr1.forEach((item)=>{
+//     console.log(item, "foreach")
+// })
+
+// fruitsArr1.map((ass)=>{
+//     console.log(ass,"map")
+// })
+
+const numbers = [1,2,3,4,5,6,7,8];
+
+numbers.filter((iyem)=>{
+    console.log(iyem % 2===0)
+})
